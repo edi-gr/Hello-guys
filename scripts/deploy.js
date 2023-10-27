6b22fa6e2bd98451aff3ffcd5aec1c5fcbeb4022
edi_gr <f20211247@goa.bits-pilani.ac.in>
2023-10-27 15:27:41 +0530

    op phoda

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+//0x5fbdb2315678afecb367f032d93f642f64180aa3;
+
+async function main() {
+  const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
+  const crowdFunding = await CrowdFunding.deploy();
+
+  await crowdFunding.deployed();
+
+  console.log(`CrowdFunding deployed to ${crowdFunding.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
